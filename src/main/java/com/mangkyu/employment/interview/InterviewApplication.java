f3496ed776f54f8b7aae252aa93e114f34f98f23
MangKyu <whalsrb1226@gmail.com>
2021-12-05 20:56:12 +0900

    First Commit

---
+++ src/main/java/com/mangkyu/employment/interview/InterviewApplication.java
@@ -0,0 +1,13 @@
+package com.mangkyu.employment.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewApplication.class, args);
+	}
+
+}
