705e70651c3fef56c84274c0c58aea715cd1c325
MangKyu <whalsrb1226@gmail.com>
2021-12-09 23:05:08 +0900

    Enable Async and Scheduling

--- src/main/java/com/mangkyu/employment/interview/InterviewApplication.java
+++ src/main/java/com/mangkyu/employment/interview/InterviewApplication.java
@@ -2,7 +2,11 @@ package com.mangkyu.employment.interview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class InterviewApplication {
 
