3d235ffe491310da2cbfcfd9b8a7d1f05cc06b0d
MangKyu <whalsrb1226@naver.com>
2021-12-26 00:05:57 +0900

    Add AddUser ViewPage

---
+++ src/main/java/com/mangkyu/employment/interview/app/user/controller/WebUserController.java
@@ -0,0 +1,28 @@
+package com.mangkyu.employment.interview.app.user.controller;
+
+import com.mangkyu.employment.interview.app.quiz.constants.QuizConstants;
+import com.mangkyu.employment.interview.enums.common.EnumMapperKey;
+import com.mangkyu.employment.interview.enums.factory.EnumMapperFactory;
+import com.mangkyu.employment.interview.enums.value.QuizLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class WebUserController {
+
+    private final EnumMapperFactory factory;
+
+    @GetMapping("/user/addView")
+    public String addUserPage(final Model model) {
+        model.addAttribute("quizLevelList", factory.get(EnumMapperKey.QUIZ_LEVEL));
+        model.addAttribute("quizCategoryList", factory.get(EnumMapperKey.QUIZ_CATEGORY));
+        model.addAttribute("quizDayList", factory.get(EnumMapperKey.QUIZ_DAY));
+        model.addAttribute("minQuizSize", QuizConstants.MINIMUM_QUIZ_SIZE);
+        model.addAttribute("maxQuizSize", QuizConstants.MAXIMUM_QUIZ_SIZE);
+        return "user/addUser";
+    }
+
+}
---
+++ src/main/resources/static/js/user/add.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+
+    $("#btn-add-user").click(function () {
+        addUser();
+    });
+
+});
+
+function addUser() {
+    const quizLevel = $('#quiz-level').val(),
+        email = $("#email").val(),
+        quizSize = $("#quizSize").val(),
+        quizDaySet = findQuizDaySet(),
+        quizCategorySet = findQuizCategorySet();
+
+    if (!quizLevel) {
+        alert("Input quizLevel");
+        return false;
+    } else if (!email) {
+        alert("Input email");
+        return false;
+    } else if (!quizSize) {
+        alert("Input quizSize");
+        return false;
+    } else if (quizDaySet.length <= 0) {
+        alert("Select quizDaySet");
+        return false;
+    } else if (quizCategorySet.length <= 0) {
+        alert("Select quizCategorySet");
+        return false;
+    }
+
+    $.ajax({
+        url: '/user',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            'email': email,
+            'quizLevel': quizLevel,
+            'quizSize': quizSize,
+            'quizDaySet': quizDaySet,
+            'quizCategorySet': quizCategorySet
+        }),
+        success: function () {
+            alert("사용자 추가 성공")
+        },
+        error: function () {
+            alert("사용자 추가 실패");
+        }
+    });
+}
+
+function findQuizDaySet() {
+    const quizDaySet = [];
+
+    $("#quiz-day-set input").each(function () {
+        const quizDay = $(this);
+        if (quizDay.is(':checked')) {
+            quizDaySet.push(quizDay.val())
+        }
+    });
+    return quizDaySet;
+}
+
+function findQuizCategorySet() {
+    const quizCategorySet = [];
+
+    $("#quiz-category-set input").each(function () {
+        const quizCategory = $(this);
+        if (quizCategory.is(':checked')) {
+            quizCategorySet.push(quizCategory.val())
+        }
+    });
+
+    return quizCategorySet;
+}
