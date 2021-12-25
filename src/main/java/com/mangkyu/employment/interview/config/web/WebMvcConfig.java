7242523f593b70df09c72b50b014455bc7e50e8d
MangKyu <whalsrb1226@naver.com>
2021-12-26 00:05:15 +0900

    Add Thymeleaf dependency and config

---
+++ src/main/java/com/mangkyu/employment/interview/config/web/WebMvcConfig.java
@@ -0,0 +1,35 @@
+package com.mangkyu.employment.interview.config.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/", "classpath:/public/", "classpath:/",
+            "classpath:/resources/", "classpath:/META-INF/resources/", "classpath:/META-INF/resources/webjars/" };
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // /에 해당하는 url mapping을 /common/test로 forward한다.
+        registry.addViewController( "/" ).setViewName( "forward:/user/addView" );
+        // 우선순위를 가장 높게 잡는다.
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    @Bean
+    public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+        return new ResourceUrlEncodingFilter();
+    }
+
+}
