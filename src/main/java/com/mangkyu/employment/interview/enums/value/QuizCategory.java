82d27e592a41d42ed33c0c977ce2fb1ed435f670
MangKyu <whalsrb1226@naver.com>
2021-12-27 21:34:01 +0900

    Modify enum contents

--- src/main/java/com/mangkyu/employment/interview/enums/value/QuizCategory.java
+++ src/main/java/com/mangkyu/employment/interview/enums/value/QuizCategory.java
@@ -10,7 +10,7 @@ public enum QuizCategory implements EnumMapperType {
 
     JAVA("Java", "Java 언어와 관련된 면접 질문을 살펴보세요"),
     SPRING("Spring Framework", "Spring 프레임워크와 관련된 면접 질문을 살펴보세요"),
-    SERVER("Server Side Programming", ""),
+    SERVER("Server Programming", "서버 프로그래밍 관련된 면접 질문을 살펴보세요"),
     NETWORK("Network", "네트워크와 관련된 면접 질문을 살펴보세요"),
     OPERATING_SYSTEM("Operating System", "운영체제와 관련된 면접 질문을 살펴보세요"),
     DATABASE("Database", "데이터베이스와 관련된 면접 질문을 살펴보세요"),
@@ -18,8 +18,8 @@ public enum QuizCategory implements EnumMapperType {
     DATA_STRUCTURE("Data Structure", "자료구조와 관련된 면접 질문을 살펴보세요"),
     ALGORITHM("Database", "알고리즘과 관련된 면접 질문을 살펴보세요"),
     PROBLEM_SOLVING("Problem Solving", "문제 해결과 관련된 면접 질문을 살펴보세요"),
-    CULTURE("Culture", "문화 적합도와 관련된 면접 질문을 살펴보세요"),
-    EXPERIENCE("Experience", "개인의 경험와 관련된 면접 질문을 살펴보세요"),
+    CULTURE("Culture Fit", "문화 적합도와 관련된 면접 질문을 살펴보세요"),
+    EXPERIENCE("Personal Experience", "개인의 경험와 관련된 면접 질문을 살펴보세요"),
     PERSONALITY("Personality", "개인의 성향과 관련된 면접 질문을 살펴보세요"),
     ;
 
