e61bac641f7c96fa7fec29e476ce3b81ac575b00
MangKyu <whalsrb1226@naver.com>
2022-01-07 01:04:37 +0900

    Add Answer Delete button and script

--- src/main/resources/static/js/quiz/edit.js
+++ src/main/resources/static/js/quiz/edit.js
@@ -13,6 +13,10 @@ $(document).ready(function () {
     $("#btn-edit-answer").click(function () {
         editAnswer(editor);
     });
+
+    $("#btn-delete-answer").click(function () {
+        deleteAnswer();
+    });
 });
 
 function editAnswer(editor) {
@@ -23,6 +27,7 @@ function editAnswer(editor) {
         alert("Input description");
         return false;
     }
+
     $.ajax({
         url: '/answer',
         type: 'PUT',
@@ -32,11 +37,33 @@ function editAnswer(editor) {
             'description': description
         }),
         success: function () {
-            $("#answerResourceId").val('a');
             alert("설명 추가 성공")
+            window.location = window.location
         },
         error: function (e) {
             alert("설명 추가 실패");
+        }
+    });
+}
+
+function deleteAnswer() {
+    const answerResourceId = $("#answerResourceId").val();
+
+    if (!answerResourceId) {
+        alert("Answer Not Exists");
+        return false;
+    }
+
+    $.ajax({
+        url: '/answer/' + answerResourceId,
+        type: 'DELETE',
+        contentType: 'application/json',
+        success: function () {
+            alert("설명 삭제 성공");
+            window.location = window.location
+        },
+        error: function () {
+            alert("설명 삭제 실패");
         }
     });
 }
