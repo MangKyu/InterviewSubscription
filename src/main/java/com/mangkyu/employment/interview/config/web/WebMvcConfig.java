f8092c9701a2a221057c0ee8110156ebd0dbfd5d
MangKyu <whalsrb1226@naver.com>
2022-01-09 00:06:58 +0900

    Add CorsConfig

---
+++ src/main/java/com/mangkyu/employment/interview/config/web/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.mangkyu.employment.interview.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+    }
+
+}
