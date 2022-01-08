2feac6222791914f06f0adc0f52dd817ced00601
MangKyu <whalsrb1226@naver.com>
2022-01-09 00:29:21 +0900

    Modify WebQuizController

--- src/main/java/com/mangkyu/employment/interview/app/quiz/controller/WebQuizController.java
+++ src/main/java/com/mangkyu/employment/interview/app/quiz/controller/WebQuizController.java
@@ -2,14 +2,12 @@ package com.mangkyu.employment.interview.app.quiz.controller;
 
 import com.mangkyu.employment.interview.app.answer.dto.GetAnswerResponse;
 import com.mangkyu.employment.interview.app.answer.service.AnswerService;
-import com.mangkyu.employment.interview.app.common.erros.exception.QuizException;
 import com.mangkyu.employment.interview.app.quiz.dto.GetQuizResponse;
 import com.mangkyu.employment.interview.app.quiz.service.QuizService;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -21,7 +19,7 @@ public class WebQuizController {
     private final AnswerService answerService;
 
     @GetMapping("/quiz/editView/{resourceId}")
-    public String addUserPage(@PathVariable final String resourceId, final Model model) throws QuizException {
+    public String addUserPage(@PathVariable final String resourceId, final Model model) {
         final GetQuizResponse quiz = quizService.getQuiz(resourceId);
         model.addAttribute("quiz", quiz);
 
