1975b72f0e518e4db2bb0d967820f3c20b0b408f
MangKyu <whalsrb1226@naver.com>
2022-02-01 04:15:29 +0900

    Add expose on Enums

--- src/main/java/com/mangkyu/employment/interview/enums/value/QuizLevel.java
+++ src/main/java/com/mangkyu/employment/interview/enums/value/QuizLevel.java
@@ -8,12 +8,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum QuizLevel implements EnumMapperType {
 
-    NEW("New", "신입"),
-    JUNIOR("Junior", "주니어"),
-    SENIOR("Senior", "시니어"),
+    NEW("New", "신입", true),
+    JUNIOR("Junior", "주니어", true),
+    SENIOR("Senior", "시니어", true),
     ;
 
     private final String title;
     private final String desc;
+    private final boolean expose;
 
 }
